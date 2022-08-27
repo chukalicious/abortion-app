31d1345230360972bab741df160a5f2300d5dfb6
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-08-26 21:08:10 -0700

    Created footer and header common components and imported them to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import { useEffect } from "react";
+import axios from "axios";
+import { config, baseUrl, minorsEndpoint } from "./axiosConfig";
+
+import Header from "./common/Header";
+import UserInfoForm from "./components/UserInfoForm";
+import Footer from "./common/Footer";
 function App() {
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}${minorsEndpoint}`, config)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <UserInfoForm />
+      <Footer />
     </div>
   );
 }
