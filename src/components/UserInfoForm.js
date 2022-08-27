48aca8c61b3b801939703eb7d334bf2c677b1edc
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-08-27 02:11:04 -0700

    deleted unneccessary const

--- src/components/UserInfoForm.js
+++ src/components/UserInfoForm.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { states } from "../data/states";
 import { Combobox } from "@headlessui/react";
-import { set, useForm } from "react-hook-form";
 
 const UserInfoForm = () => {
   const [errors, setErrors] = useState({
