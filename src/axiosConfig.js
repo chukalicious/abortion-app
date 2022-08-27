6faf12f540f319c3f518c0245321f23847e902c3
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-08-26 20:43:34 -0700

    added config file for axios

---
+++ src/axiosConfig.js
@@ -0,0 +1,10 @@
+export const config = {
+  headers: {
+    token: process.env.REACT_APP_API_KEY,
+    "Access-Control-Allow-Origin": "*",
+  },
+};
+
+export const baseUrl = "https://api.abortionpolicyapi.com/v1/";
+export const insuranceEndpoint = "insurance_coverage/states/";
+export const minorsEndpoint = "minors/states/";
