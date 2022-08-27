facf91e038c063a9bcdd4b51e885ed0acb907724
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-08-27 02:10:32 -0700

    manually created form validation, since I had trouble implmenting react form hooks

--- src/components/UserInfoForm.js
+++ src/components/UserInfoForm.js
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import { states } from "../data/states";
 import { Combobox } from "@headlessui/react";
+import { set, useForm } from "react-hook-form";
 
 const UserInfoForm = () => {
+  const [errors, setErrors] = useState({
+    ageErrorMs: "",
+    stateErrorMs: "",
+  });
+  console.log("errors", errors);
+
   const [selectedState, setSelectedState] = useState(states);
   const [query, setQuery] = useState("");
   const [formValues, setFormValues] = useState({
@@ -18,10 +25,10 @@ const UserInfoForm = () => {
         });
 
   const handleChange = (e) => {
-    setFormValues({ ...formValues, age: e.target.value, state: "" });
+    setFormValues({ ...formValues, age: e.target.value, state: query });
   };
 
-  const handleSubmit = (e) => {
+  const katSubmit = (e) => {
     e.preventDefault();
     setSelectedState("");
     setFormValues({
@@ -34,14 +41,50 @@ const UserInfoForm = () => {
     setQuery(e.target.value);
   };
 
+  const validateState = () => {
+    if (query === "") {
+      setErrors({
+        ...errors,
+        stateErrorMs: "*Please enter a valid state name*",
+      });
+    } else {
+      setErrors({ ...errors, stateErrorMs: "" });
+    }
+  };
+
+  const validateAge = () => {
+    if (formValues.age === "") {
+      setErrors({
+        ...errors,
+        ageErrorMs:
+          "*Enter an age value to obtain information on retrictions by patient age*",
+      });
+    } else if (formValues.age < 0 || formValues.age > 99) {
+      setErrors({
+        ...errors,
+        ageErrorMs: "*Enter an age between 0 and 99*",
+      });
+    } else {
+      setErrors({
+        ...errors,
+        stateErrorMs: "",
+        ageErrorMs: "",
+      });
+    }
+  };
+
   return (
     <div className="flex flex-col w-full">
-      <form onSubmit={handleSubmit} className="flex flex-col w-[85%] mx-auto">
+      <form onSubmit={katSubmit} className="flex flex-col w-[85%] mx-auto">
         <Combobox value={selectedState} onChange={setSelectedState}>
           <label htmlFor="state" className="w-[50%] text-left ml-[25%]">
             Select your state:
           </label>
-          <Combobox.Input onChange={handleQuery} className="w-[50%] mx-auto" />
+          <Combobox.Input
+            onChange={handleQuery}
+            className="w-[50%] mx-auto"
+            onBlur={validateState}
+          />
           <Combobox.Options>
             {filteredStates.map((state, i) => (
               <Combobox.Option key={i} value={state}>
@@ -49,19 +92,26 @@ const UserInfoForm = () => {
               </Combobox.Option>
             ))}
           </Combobox.Options>
+          {errors.stateErrorMs !== "" ? (
+            <p className="text-red-500">{errors.stateErrorMs}</p>
+          ) : null}
         </Combobox>
 
         <label htmlFor="age" className="w-[50%] text-left ml-[25%]">
           Your Age:{" "}
         </label>
         <input
-          type="text"
+          type="number"
           name="age"
           id="age"
           value={formValues.age}
           onChange={handleChange}
           className="w-[50%] mx-auto"
+          onBlur={validateAge}
         />
+        {errors.ageErrorMs !== "" ? (
+          <p className="text-red-500">{errors.ageErrorMs}</p>
+        ) : null}
         <button type="submit">submit</button>
       </form>
     </div>
