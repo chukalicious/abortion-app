c976e9375d11240efd8eab10538b6b0a7c09c00b
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-09-18 14:51:33 -0700

    installed and set up daisyUI and theme-change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
-
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 import Header from "./common/Header";
 import Footer from "./common/Footer";
 import StateOptionContainer from "./components/StateOption";
 function App() {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
   return (
     <div className="App">
       <Header />
