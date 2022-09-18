33dd17f4bed1d51aac42e05659a6755bd7b869a4
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-09-18 16:52:59 -0700

    added footer and header components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
 import Header from "./common/Header";
-import Footer from "./common/Footer";
 import StateOptionContainer from "./components/StateOption";
+import Footer from "./common/Footer";
 function App() {
   useEffect(() => {
     themeChange(false);
   }, []);
   return (
-    <div className="App">
+    <div>
       <Header />
       <StateOptionContainer />
       <Footer />
