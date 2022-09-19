957066fb68eabae6ed838346df097f0adffd58fd
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-09-19 00:17:19 -0700

    git rid of icons

--- src/components/StateOption/StateOption.js
+++ src/components/StateOption/StateOption.js
@@ -16,9 +16,10 @@ const StateOption = (props) => {
   const defaultedValue = "Select your State";
 
   return (
-    <>
+    <div className="flex w-full flex-col">
       {props.stateName && (
         <form
+          className="w-full flex flex-col "
           onSubmit={(e) => {
             e.preventDefault();
             console.log("submitted!");
@@ -27,7 +28,7 @@ const StateOption = (props) => {
         >
           <select
             name="selectedState"
-            className="select select-primary w-full max-w-xs"
+            className="select select-primary w-full max-w-xs mx-auto"
             onChange={handleChange}
           >
             <option disabled selected>
@@ -39,12 +40,10 @@ const StateOption = (props) => {
               </option>
             ))}
           </select>
-          <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
-            Search
-          </button>
+          <button className="btn sm:btn-sm md:btn-md lg:btn-lg">Search</button>
         </form>
       )}
-    </>
+    </div>
   );
 };
 
