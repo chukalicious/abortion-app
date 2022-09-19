49a3601980317df7e6730c9761846eb2426e78cd
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-09-19 03:34:14 -0700

    created case cases for the gestational limits

--- src/components/StateResults/GestationalLimits.js
+++ src/components/StateResults/GestationalLimits.js
@@ -5,14 +5,21 @@ const GestationalLimits = (props) => {
       <p>
         Allows {props.gestationalLimits.exception_health} health exception.{" "}
       </p>
-      <p>
-        Banned after {props.gestationalLimits.banned_after_weeks_since_LMP}{" "}
-        weeks since last menstrual period.{" "}
-      </p>
+      {props.gestationalLimits.banned_after_weeks_since_LMP === 99 ? (
+        <p>Abortions are banned after viability in this state</p>
+      ) : props.gestationalLimits.banned_after_weeks_since_LMP === 28 ? (
+        <p>Abortions are banned after the third trimester </p>
+      ) : props.gestationalLimits.banned_after_weeks_since_LMP === 22 ? (
+        <p>Abortions are banned post-fertilization</p>
+      ) : props.gestationalLimits.banned_after_weeks_since_LMP === 0 ? (
+        <p>Unknown Value</p>
+      ) : (
+        <p>Abortions are banned post-implantation</p>
+      )}
       <p>
         {props.gestationalLimits.exception_life === true
-          ? "Allows exceptions for life threatening conditions"
-          : "Does not allow exceptions for life threatening conditions"}{" "}
+          ? "There is no abortion ban for life threatening conditions"
+          : "Does not allow exceptions in case there are life threatining conditions ot the mother"}{" "}
       </p>
     </div>
   );
