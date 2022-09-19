333118c17101a6bb21404bcf391d641932020653
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-09-19 00:12:24 -0700

    chnaged config file to correct darltheme value

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
+
 import Header from "./common/Header";
 import StateOptionContainer from "./components/StateOption";
 import Footer from "./common/Footer";
