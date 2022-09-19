db2030189ebdbeacd7efca999e323d0819de050a
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-09-19 16:50:21 -0700

    Have added edge cases to the GestationalLimits component.

--- src/components/StateResults/GestationalLimits.js
+++ src/components/StateResults/GestationalLimits.js
@@ -2,25 +2,52 @@ const GestationalLimits = (props) => {
   console.log("GestationalLimits: props: ", props);
   return (
     <div>
-      <p>
-        Allows {props.gestationalLimits.exception_health} health exception.{" "}
-      </p>
-      {props.gestationalLimits.banned_after_weeks_since_LMP === 99 ? (
-        <p>Abortions are banned after viability in this state</p>
-      ) : props.gestationalLimits.banned_after_weeks_since_LMP === 28 ? (
-        <p>Abortions are banned after the third trimester </p>
-      ) : props.gestationalLimits.banned_after_weeks_since_LMP === 22 ? (
-        <p>Abortions are banned post-fertilization</p>
-      ) : props.gestationalLimits.banned_after_weeks_since_LMP === 0 ? (
-        <p>Unknown Value</p>
+      {props.gestationalLimits.exception_health ? (
+        <p>
+          Allows {props.gestationalLimits.exception_health.toLowerCase()} health
+          exception(s).{" "}
+        </p>
       ) : (
-        <p>Abortions are banned post-implantation</p>
+        <p>
+          This state does not make exceptions for life threatening health
+          conditions to the pregnant person.{" "}
+        </p>
       )}
-      <p>
-        {props.gestationalLimits.exception_life === true
-          ? "There is no abortion ban for life threatening conditions"
-          : "Does not allow exceptions in case there are life threatining conditions ot the mother"}{" "}
-      </p>
+      {props.gestationalLimits.banned_after_weeks_since_LMP === 99 ? (
+        <p>
+          In this state abortions are banned after the fetus has reached
+          viability.
+        </p>
+      ) : props.gestationalLimits.banned_after_weeks_since_LMP === 28 ? (
+        <p>Abortions are banned after the third trimester. </p>
+      ) : props.gestationalLimits.banned_after_weeks_since_LMP === 22 ? (
+        <p>Abortions are banned "post-fertilization".</p>
+      ) : props.gestationalLimits.banned_after_weeks_since_LMP === 0 ? (
+        <p>Cannot interpret rules</p>
+      ) : (
+        <p>Abortions are banned "post-implantation".</p>
+      )}
+      {props.gestationalLimits.exception_life === true ? (
+        <p>
+          Abortion exceptions are granted if necessary to save the pregnant
+          person's life.{" "}
+        </p>
+      ) : (
+        <p>
+          Abortion is banned even in case the pregnant person's life is in
+          danger.{" "}
+        </p>
+      )}{" "}
+      {props.gestationalLimits.exception_fetal &&
+      props.gestationalLimits.exception_fetal !== "" ? (
+        <p>
+          Exceptions may be granted if the fetus suffers a{" "}
+          {props.gestationalLimits.exception_fetal.toLowerCase()}.{" "}
+        </p>
+      ) : null}
+      {props.gestationalLimits.exception_rape_or_incest ? (
+        <p>Exceptions may be granted in cases of rape or incest</p>
+      ) : null}
     </div>
   );
 };
