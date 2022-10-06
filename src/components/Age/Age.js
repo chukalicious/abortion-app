5da817482966e3d06b64e81b4f4197821747d380
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-10-06 03:31:09 -0700

    added local state to the Age.js, implemented controlled inputs, made todo comments and guides

--- src/components/Age/Age.js
+++ src/components/Age/Age.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
+// must hold local state to use user input
+
 const Age = () => {
+  const [age, setAge] = useState("");
+
+  console.log("age: ", age);
+
+  const handleChange = (e) => {
+    setAge(e.target.value);
+  };
   return (
     <div className="w-[95%] mx-auto flex flex-col">
-      {" "}
+      <h2 className="text-xl">
+        If you want to check whether minor restrictions apply, please enter an
+        age below:
+      </h2>
+
       <form className="mx-auto flex flex-col w-full">
-        <h2 className="text-xl">Enter your Age:</h2>
+        {/* must have validation to accept only numbers */}
 
         <input
           type="text"
           placeholder="Type here"
+          name="age"
+          value={age}
+          onChange={handleChange}
           className="input input-bordered input-lg w-full max-w-xs input-secondary"
         />
       </form>
