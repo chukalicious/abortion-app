fe8b2a1b0cfd4ecca66094884c35a757ad9694ee
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-10-11 00:04:21 -0700

    Created the function to collect user input onBlur event. The state is held locally and then passed up to its parent component via getAge() function. The AgeContainer.js now holds that value and it can be used as props to a child component that can compare the age with minors’ rules.

--- src/components/Age/Age.js
+++ src/components/Age/Age.js
@@ -2,13 +2,20 @@ import { useState } from "react";
 
 // must hold local state to use user input
 
-const Age = () => {
-  const [age, setAge] = useState("");
+const Age = (props) => {
+  console.log("Age: props: ", props);
 
-  console.log("age: ", age);
+  const [age, setAge] = useState("");
+  console.log("Age: age: ", age);
 
   const handleChange = (e) => {
     setAge(e.target.value);
+  };
+
+  const handleBlur = (e) => {
+    if (age !== "") {
+      props.getAge(age);
+    }
   };
   return (
     <div className="w-[95%] mx-auto flex flex-col">
@@ -18,9 +25,10 @@ const Age = () => {
       </h2>
 
       <form className="mx-auto flex flex-col w-full">
-        {/* must have validation to accept only numbers */}
+        {/* must have validation to accept only numbers, use regex */}
 
         <input
+          onBlur={handleBlur}
           type="text"
           placeholder="Type here"
           name="age"
--- src/components/Age/AgeContainer.js
+++ src/components/Age/AgeContainer.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Age from "./Age";
 
 const AgeContainer = (props) => {
-  console.log(props);
+  console.log("AgeContainer.js: props: ", props);
 
-  return <Age minorsData={props.minorsData} />;
+  const [userAge, setUserAge] = useState("");
+  console.log("AgeContainer: userAge: ", userAge);
+
+  const getAge = (age) => {
+    setUserAge(Number(age));
+  };
+
+  return <Age minorsData={props.minorsData} getAge={getAge} />;
 };
 
 export default AgeContainer;
