c62d49380c1d63f510dfdf9fd8bfe561edc2e84c
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-10-12 23:38:33 -0700

    moved the minors component up the list of rules to have it closer to the age field

--- src/components/StateResults/StateResults.js
+++ src/components/StateResults/StateResults.js
@@ -26,6 +26,17 @@ const StateResults = (props) => {
       <h2 className="pt-10 text-xl font-normal pl-4">
         State Rules for the state of {props.stateName}{" "}
       </h2>{" "}
+      {rules.minors && (
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">Minors</div>
+          <div className="collapse-content">
+            <Minors minors={rules.minors} />
+          </div>
+        </div>
+      )}
       {rules.gestationalLimits && (
         <div
           tabIndex={0}
@@ -36,17 +47,6 @@ const StateResults = (props) => {
           </div>
           <div className="collapse-content">
             <GestationalLimits gestationalLimits={rules.gestationalLimits} />
-          </div>
-        </div>
-      )}
-      {rules.minors && (
-        <div
-          tabIndex={0}
-          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
-        >
-          <div className="collapse-title text-xl font-medium">Minors</div>
-          <div className="collapse-content">
-            <Minors minors={rules.minors} />
           </div>
         </div>
       )}
