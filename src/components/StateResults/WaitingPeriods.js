d30faf96a8af44b499cc6a4018bb1c97d9c8750e
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-11-29 21:54:36 -0800

    used cards to display the text information in the waiting periods component

--- src/components/StateResults/WaitingPeriods.js
+++ src/components/StateResults/WaitingPeriods.js
@@ -1,24 +1,49 @@
 const WaitingPeriods = (props) => {
   console.log("WaitingPeriods: props: ", props);
   return (
-    <div>
+    <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
       {props.waitingPeriods.waiting_period_hours > 0 ? (
-        <p>
-          There's a {props.waitingPeriods.waiting_period_hours}-hour waiting
-          period requirement in this state.
-        </p>
+        <div className="carousel-item">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <p>
+                There's a {props.waitingPeriods.waiting_period_hours}-hour
+                waiting period requirement in this state.
+              </p>
+            </div>
+          </div>
+        </div>
       ) : (
-        <p>There's no waiting period required. </p>
+        <div className="carousel-item">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <p>There's no waiting period required. </p>
+            </div>
+          </div>
+        </div>
       )}
       {props.waitingPeriods.counseling_visits &&
       props.waitingPeriods.counseling_visits === 1 ? (
-        <p>Counseling is required by the state. </p>
+        <div className="carousel-item">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <p>Counseling is required by the state. </p>
+            </div>
+          </div>
+        </div>
       ) : props.waitingPeriods.counseling_visits &&
         props.waitingPeriods.counseling_visits === 2 ? (
-        <p>
-          Abortion counseling or ultrasound must be obtained at the facility
-          before the waiting period begins, requiring two trips to the clinic
-        </p>
+        <div className="carousel-item">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <p>
+                Abortion counseling or ultrasound must be obtained at the
+                facility before the waiting period begins, requiring two trips
+                to the clinic
+              </p>
+            </div>
+          </div>
+        </div>
       ) : null}
     </div>
   );
